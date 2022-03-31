31c19692abbc442370c32190c2fcc86404c0e8a4
tomerbug <97694931+tomerbug@users.noreply.github.com>
2022-03-31 14:54:22 +0300

    Update MyFlask.py

--- MyFlask.py
+++ MyFlask.py
@@ -28,7 +28,7 @@ app = Flask(__name__)
 #Home/login PAGE
 @app.route('/',methods =['POST','GET'])
 def home_page():
-    return render_template("Home.html")
+    return render_template("Flights.html")
 #consider as register for now
 def login():
     password = request.form['password']
@@ -76,19 +76,16 @@ def post_new():
     return 'OK'
 
 # @app.route('/delete/<int:x>',methods = ['DELETE'])
-@app.route('/delete', methods = ['GET','DELETE'])
+@app.route('/delete/<int:x>', methods = ['DELETE'])
 # def delete_user(x):
-def delete_user():
+def delete_user(x):
      conn, cursor = SQL_connect.Dbinit()
      par = ""
      cursor = SQL_connect.delete_user(x, cursor)
-     for row in crusor:
-         z = str(row)
-         par = z + par
      SQL_connect.close_sql(conn)
      return par
-
-@app.route('/put/<int x>/<str name>', methods = ['PUT'])
+#put user
+@app.route('/put/<string:name>', methods = ['PUT'])
 def put_user(x,name):
      conn, cursor = SQL_connect.Dbinit()
      par = ""
@@ -98,4 +95,109 @@ def put_user(x,name):
          par = z + par
      SQL_connect.close_sql(conn)
      return par
+#########################flights###################
+#get flights
+@app.route('/form/flight',methods = ['GET'])
+def form():
+    #function for the form -flight post
+    return render_template('flights.html')
+
+    return render_template('flights.html')
+@app.route('/flights/get', methods = ['GET'])
+def get_f():
+    conn, cursor = SQL_connect.Dbinit()
+    par = ""
+    cursor = SQL_connect.get_flights(cursor)
+    for row in cursor:
+        z = str(row)
+        par = z + par
+    SQL_connect.close_sql(conn)
+    return par
+#get flights/id
+@app.route('/flights/get/<int:x>', methods = ['GET'])
+def get_f_id_flask(x):
+    conn, cursor = SQL_connect.Dbinit()
+    par = ""
+    cursor = SQL_connect.get_f_id(cursor,x)
+    for row in cursor:
+        z = str(row)
+        par = z + par
+    SQL_connect.close_sql(conn)
+    return par
+@app.route('/other', methods = ['GET'])
+def exe():
+
+    return "ok"
+#post flights
+@app.route('/flights/add', methods = ['POST'])#לא סיימתי!!!!
+def get_post_flight_flask():
+    conn , cursor = SQL_connect.Dbinit()
+    time = request.form['TIMESTAMP']
+    seats = request.form['REMAINMGSEATS']
+    origin_country = request.form['ORIGIONCOUNTRYID']
+    dest_country = request.form['ORIGIONCOUNTRYID']
+    s = SQL_connect.post_flights(crusor, time, seats, origin_country,dest_country)
+    SQL_connect.close_sql(conn)
+    return  "namda"
+###put flights
+@app.route('/flight/put/<int:x>', methods = ['PUT'])
+def put_flight(x):
+     conn, cursor = SQL_connect.Dbinit()
+     par = ""
+     cursor = SQL_connect.put_flight(cursor,x)
+     for row in crusor:
+         z = str(row)
+         par = z + par
+     SQL_connect.close_sql(conn)
+     return par
+#delete flights
+@app.route('/flights/delete/<int:x>', methods = ['DELETE'])
+def delete_flight_f(x):
+    conn, cursor = SQL_connect.Dbinit()
+    cursor =SQL_connect.delete_flight(cursor, x)
+    SQL_connect.close_sql(conn)
+    return "done"
+
+###############tickets#############
+#get tickets
+@app.route('/tickets', methods = ['GET'])
+def get_t():
+    conn, cursor = SQL_connect.Dbinit()
+    par = ""
+    cursor = SQL_connect.get_tickets(cursor)
+    for row in cursor:
+        z = str(row)
+        par = z + par
+    SQL_connect.close_sql(conn)
+    return par
+#get ticket id
+@app.route('/tickets/get/<int:x>', methods = ['GET'])#לא בדקתי עדיין
+def get_t_id():
+    conn, cursor = SQL_connect.Dbinit()
+    par = ""
+    cursor = SQL_connect.get_tickets_id(cursor,x)
+    for row in cursor:
+        z = str(row)
+        par = z + par
+    SQL_connect.close_sql(conn)
+    return par
+@app.route('/tickets/post', methods = ['POST'])#לא בדקתי עדיין
+def post_t():
+    conn, cursor = SQL_connect.Dbinit()
+    par = ""
+    userID = request.form['User Id']
+    flightID = request.form['flight Id']
+    cursor = SQL_connect.post_tickets_sql(cursor,userID,flightID)
+    for row in cursor:
+        z = str(row)
+        par = z + par
+    SQL_connect.close_sql(conn)
+    return par
+#DELETE TICKET
+@app.route('/tickets/delete/<int:x>', methods = ['DELETE'])
+def ticket_delete_f(x):
+    conn, cursor = SQL_connect.Dbinit()
+    cursor =SQL_connect.delete_ticket(cursor, x)
+    SQL_connect.close_sql(conn)
+    return "deleted"
 app.run()
